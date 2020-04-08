0973fd6d3587bc88651c2e0cd31e914f80721d35
kevin.li <kevin.li@duethealth.com>
2020-04-07 17:24:03 -0700

    Added trees

---
+++ Tree/Tree.playground/Sources/TreeNode.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+public class TreeNode<T> {
+
+    public var value: T
+    public var children: [TreeNode] = []
+
+    public init(_ value: T) {
+        self.value = value
+    }
+
+}
+
+// MARK: Add Operations
+extension TreeNode {
+
+    public func add(_ child: TreeNode) {
+        children.append(child)
+    }
+
+}
+
+// MARK: Traversal Operations
+extension TreeNode {
+
+    public func forEachDepthFirst(visit: (TreeNode) -> Void) {
+        visit(self)
+        children.forEach {
+            $0.forEachDepthFirst(visit: visit)
+        }
+    }
+
+    public func forEachLevelOrder(visit: (TreeNode) -> Void) {
+        visit(self)
+        var queue = Queue<TreeNode>()
+        children.forEach { queue.enqueue($0) }
+        while let node = queue.dequeue() {
+            visit(node)
+            node.children.forEach { queue.enqueue($0) }
+        }
+    }
+
+}
+
+// MARK: Search Operations
+extension TreeNode where T: Equatable {
+
+    public func search(_ value: T) -> TreeNode? {
+        var result: TreeNode?
+        forEachLevelOrder { node in
+            if node.value == value {
+                result = node
+            }
+        }
+        return result
+    }
+
+}
